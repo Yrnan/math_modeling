68147b81ce5dd47848e3aac55d719d2b181309c6
Yrnan <75009121+Yrnan@users.noreply.github.com>
2023-10-03 14:26:17 +0200

    готово

---
+++ Lec_2_if_elif_else.py
@@ -0,0 +1,20 @@
+a = 3
+if a > 5:
+    print("hello 5")
+elif a < 2:
+    print("hello 2")
+else:
+    print("tupo hello")
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Lec_2_if_else.py
@@ -0,0 +1,14 @@
+a = 3
+if a > 4:
+    print(f"hello 4")
+else:
+    print(f"hello {a}")
+
+
+
+
+
+
+
+
+
