23373e382171a05b9c19b970b6620bfd59c7530c
Yrnan <75009121+Yrnan@users.noreply.github.com>
2023-11-07 14:46:31 +0200

    les_3

---
+++ Lec_3_from.py
@@ -0,0 +1,10 @@
+from Lec_3_my_module import a, b
+print(a * b)
+
+from Lec_3_my_module import *
+
+print(c[2] * c[1])
+
+
+
+
---
+++ lec_3_import.py
@@ -0,0 +1,13 @@
+import Lec_3_my_module
+
+print(Lec_3_my_module.a)
+
+b = Lec_3_my_module.b * 3
+print(b)
+
+print(Lec_3_my_module.c[2])
+
+
+
+
+
