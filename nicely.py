1c3c676d6d31be3cc02f4585d8ae239bdc27f292
Yrnan <75009121+Yrnan@users.noreply.github.com>
2024-02-05 19:55:00 +0200

    f

--- nicely.py
+++ nicely.py
@@ -6,9 +6,10 @@ y = [7, 4, 9]
 plt.plot(x, y, color='g', label='Graf 1', marker='>', ms=5)
 plt.plot(y, x, color='r', label='Graf 2', marker='o', ms=3)
 
-plt.xlabel ('cringe: x')
-plt.ylabel ('coord: y')
-plt.legend()
-plt.title('BaZa')
-plt.grid()
+# --- Украшательства ---
+plt.xlabel('Coord: x') # Подись оси ОХ
+plt.ylabel('Coord: y') # Подпись оси ОУ
+plt.legend() # Вызов "легенды"
+plt.title('Base') # Общая подпись графика
+plt.grid() # Подключение сетки
 plt.savefig('fig_2.png')
