0d95530aed5f775127e5e2f07276b49497c57b46
Yrnan <75009121+Yrnan@users.noreply.github.com>
2024-02-20 13:57:35 +0200

    Z

---
+++ Lab_7_task_1.py
@@ -0,0 +1,35 @@
+from matplotlib.animation import FuncAnimation
+import matplotlib.pyplot as plt
+import numpy as np
+import sympy as sp
+
+def cycloid(r):
+    x = np.arange(a, b, c)
+    y = []
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
